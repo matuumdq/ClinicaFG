ea8c010387bfcb540068fc2d1cd66937a5a54d9a
matuumdq <salinasmateo@outlook.com>
2023-05-15 11:41:08 -0300

    style: :art: add real img of clinic

--- src/components/Tratamientos.jsx
+++ src/components/Tratamientos.jsx
@@ -10,31 +10,31 @@ const Tratamientos = () => {
         <h2 className="h-full w-full text-center uppercase font-mono font-bold text-xl md:text-5xl text-white">Tratamientos</h2>
       </div>
       </Parallax>
-      <div id="tratamientos" className=" max-w-screen-md mx-6 sm:mx-auto">
+      <div id="tratamientos" className=" max-w-screen-lg mx-6 sm:mx-auto">
         <h3 className="font-bold text-2xl text-center py-8 uppercase font-mono max-w-sm mx-auto">Mira todos los tratamientos que tenemos para ofrecerte</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 content-center place-items-center h-auto gap-10 py-8 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 content-center place-items-center h-auto gap-12 py-8 ">
           {tratamientos.map((tratamiento) => (
             <div key={tratamiento.id} className="flex flex-col pb-5 gap-3 border border-gray-400/40 w-full min-h-[15vh] h-auto rounded-md shadow-md">
               <div className="absolute z-10 text-white mx-4 mt-2 px-2 py-1 rounded-lg bg-amber-400/80 cursor-default">${tratamiento.price}</div>
               <img className="md:w-full rounded-t-md" src={tratamiento.img} alt={tratamiento.name} />
               <p className="mx-4 uppercase font-semibold">{tratamiento.name}</p>
-              <p className="mx-4 font-thin italic">{tratamiento.explanation}</p>
-              <a href={msg} target="_blank" rel="noreferrer" className="font-light bg-amber-400/60 mx-auto px-2 py-1 rounded-md hover:scale-105 ease-in duration-300 text-purple-700">Mas Informacion</a>
+              <p className="mx-4 font-thin italic line-clamp-3">{tratamiento.explanation}</p>
+              <a href={msg} target="_blank" rel="noreferrer" className="font-light bg-amber-400/60 mx-auto px-2 py-1 rounded-md hover:scale-105 ease-in duration-300 text-purple-700">Mas Información</a>
             </div>
           ))}
         </div>
       </div>
 
-      <div id="cursos" className=" max-w-screen-md mx-6 sm:mx-auto">
+      <div id="cursos" className=" max-w-screen-lg mx-6 sm:mx-auto">
         <h3 className="font-bold text-2xl text-center py-8 uppercase font-mono">Nuestros Cursos</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 content-center place-items-center h-auto gap-10 py-8 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 content-center place-items-center h-auto gap-12 py-8 ">
           {cursos.map((curso) => (
             <div key={curso.id} className="flex flex-col cursor-default pb-5 gap-3 border border-gray-400/40 w-full min-h-[15vh] h-auto rounded-md shadow-md">
               <div className="absolute z-10 text-white mx-4 mt-2 px-2 py-1 rounded-lg bg-amber-400/80 cursor-default">${curso .price}</div>
               <img className="object-cover w-full rounded-t-md" src={curso.img} alt={curso.name} />
               <p className="mx-4 uppercase font-semibold">{curso.name}</p>
-              <p className="mx-4 font-thin italic line-clamp-3 hover:line-clamp-none ">{curso.explanation}</p>
-              <a href={msgcurso} target="_blank" rel="noreferrer" className="font-light bg-amber-400/60 mx-auto px-2 py-1 rounded-md hover:scale-105 ease-in duration-300 text-purple-700">Mas Informacion</a>
+              <p className="mx-4 font-thin italic line-clamp-3">{curso.explanation}</p>
+              <a href={msgcurso} target="_blank" rel="noreferrer" className="font-light bg-amber-400/60 mx-auto px-2 py-1 rounded-md hover:scale-105 ease-in duration-300 text-purple-700">Mas Información</a>
             </div>
           ))}
         </div>
--- src/db.js
+++ src/db.js
@@ -64,30 +64,30 @@ export const doctors = [
 export const tratamientos = [
     {
       id: 0,
-      name: "Peeling",
-      explanation: '¡Descubre el secreto de una piel radiante! Renueva tu cutis, elimina imperfecciones y revela una apariencia rejuvenecida. ¡Dile adiós a las manchas y arrugas, y dale la bienvenida a una piel deslumbrante y llena de luminosidad!',
-      img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1683417380/trat2_g6rszk.png",
+      name: "Diagnostico Cutaneo",
+      explanation: 'Con este examen, nuestros expertos pueden evaluar la salud de la piel y detectar cualquier problema, desde manchas solares y arrugas hasta acné y otras afecciones dermatológicas. ',
+      img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1684155237/cutaneo_bzfghg.png",
       price: 250
     },
     {
       id: 1,
-      name: "Hilos Mágicos",
-      explanation: '¡Experimenta el poder de la juventud con nuestro innovador tratamiento de Hilos Tensores! Reafirma, levanta y rejuvenece tu rostro en un instante, devolviéndote una apariencia fresca, natural y sin cirugía. ¡Dale a tu piel el impulso que merece!',
-      img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1683417380/trat2_g6rszk.png",
+      name: "Dermaplaning",
+      explanation: ' Este tratamiento es ideal para aquellos que buscan mejorar la textura y el tono de la piel, reducir la apariencia de las arrugas finas y los poros dilatados y lograr un brillo saludable. ',
+      img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1684155236/dermaplaning_cvkwjl.png",
       price: 500
     },
     {
       id: 2,
-      name: "Masoterapia",
-      explanation: '¡Regálate un momento de puro deleite! Relaja cuerpo y mente mientras nuestros expertos masajistas deshacen cada nudo, liberando tensiones y devolviéndote la vitalidad que mereces. ¡Déjate consentir y renueva tu bienestar hoy mismo!',
-      img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1683417380/trat2_g6rszk.png",
+      name: "Hyaluron Pen",
+      explanation: 'Este tratamiento es ideal para aquellos que desean mejorar la apariencia de sus labios, líneas finas y arrugas sin los riesgos y tiempos de recuperación asociados con las inyecciones tradicionales. ',
+      img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1684155236/Hialuron_fwypim.png",
       price: 700
     },
     {
       id: 3,
       name: "Body Reduction",
       explanation: 'Utilizamos las últimas tecnologías y equipos en el mercado, así como técnicas manuales como masajes y drenaje linfático para ayudar a nuestros pacientes a lograr sus objetivos de pérdida de peso y moldeado corporal.',
-      img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1683417380/trat2_g6rszk.png",
+      img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1684155236/bodyred_lt8ygw.png",
       price: 900
     },
     {
@@ -109,7 +109,7 @@ export const tratamientos = [
   export const cursos = [
     {
       id: 0,
-      name: "Diplomatura en Quimica Cosmetica",
+      name: "Quimica Cosmetica",
       explanation: 'Aprenderás a formular y producir tus propios productos personalizados con la ayuda de nuestros expertos. Ideal para aquellos que buscan expandir sus conocimientos en estética o iniciar su propia línea de productos cosméticos.',
       img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1684153270/quimica_uaupt4.png",
       price: 250
@@ -130,23 +130,23 @@ export const tratamientos = [
     },
     {
       id: 3,
-      name: "MELA",
-      explanation: 'Aprende los métodos más efectivas para limpiar y revitalizar la piel, brindando a tus clientes un cutis impecable y lleno de luminosidad. ¡Destaca en el mundo de la estética y transforma vidas a través de la belleza!',
-      img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1683417380/trat2_g6rszk.png",
+      name: "Master en Peptonas",
+      explanation: 'Aprenderás sobre sus propiedades bioquímicas y cómo se aplican en tratamientos faciales y corporales para estimular la regeneración celular y mejorar la elasticidad de la piel.',
+      img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1684153270/peptonas_yqgae7.png",
       price: 900
     },
-    {
-      id: 4,
-      name: "Máster en Glúteos",
-      explanation: '¡Eleva tus habilidades en la estética corporal a un nuevo nivel! Aprende las técnicas más avanzadas para tonificar, levantar y esculpir los glúteos, brindando a tus pacientes una figura perfecta para que se sientan más seguros y atractivos.',
-      img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1683417380/trat2_g6rszk.png",
-      price: 300
-    },
-    {
-      id: 5,
-      name: "Máster en Hilos Cosmetológicos",
-      explanation: 'Transforma rostros, cambiando para bien las vidas de tus pacientes. Aprenderás a aplicar técnicas vanguardistas para reducir la flacidez facial y obtener resultados duraderos y espectaculares.',
-      img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1683417380/trat2_g6rszk.png",
-      price: 1000
-    }
+    // {
+    //   id: 4,
+    //   name: "Máster en Glúteos",
+    //   explanation: '¡Eleva tus habilidades en la estética corporal a un nuevo nivel! Aprende las técnicas más avanzadas para tonificar, levantar y esculpir los glúteos, brindando a tus pacientes una figura perfecta para que se sientan más seguros y atractivos.',
+    //   img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1683417380/trat2_g6rszk.png",
+    //   price: 300
+    // },
+    // {
+    //   id: 5,
+    //   name: "Máster en Hilos Cosmetológicos",
+    //   explanation: 'Transforma rostros, cambiando para bien las vidas de tus pacientes. Aprenderás a aplicar técnicas vanguardistas para reducir la flacidez facial y obtener resultados duraderos y espectaculares.',
+    //   img: "https://res.cloudinary.com/dnqmez68n/image/upload/v1683417380/trat2_g6rszk.png",
+    //   price: 1000
+    // }
   ];
