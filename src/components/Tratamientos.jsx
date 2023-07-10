f428e14e0dc6fdfb11fa08e409d18a9437e56790
matuumdq <salinasmateo@outlook.com>
2023-07-10 15:49:52 -0300

    style: :art: fix price in curses

--- src/components/Tratamientos.jsx
+++ src/components/Tratamientos.jsx
@@ -61,7 +61,11 @@ const Tratamientos = () => {
 				className="flex flex-col cursor-default pb-5 gap-3 border border-gray-400/40 w-full min-h-[15vh] h-auto rounded-md shadow-md"
 			>
 				<div className="absolute z-10 text-white mx-4 mt-2 px-2 py-1 rounded-lg bg-amber-400/80 cursor-default">
-					${curso.price}
+					{curso.price ? (
+						<p>${curso.price}</p>
+					) : (
+						<p>${curso.priceext} USD</p>
+					)}
 				</div>
 				<img
 					className="object-cover w-full rounded-t-md"
